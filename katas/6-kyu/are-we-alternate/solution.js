1e2f36ef673c97feb8289036659612eaab17c34a
AJAY0993 <AJAY0993@users.noreply.github.com>
2024-08-23 20:10:10 +0000

    Add katas solved on 2024/08/23

--- katas/6-kyu/are-we-alternate/solution.js
+++ katas/6-kyu/are-we-alternate/solution.js
@@ -1,1 +1,24 @@
-// codeblock not found
+function isAlt(word) {
+  // happy codinggg
+  word = word.toLowerCase()
+  const first = 'aeiou'.includes(word[0]) ? 'vowel' : 'consonant'
+  if(first == 'consonant'){
+    for(let i=0; i<word.length; i++){
+        if(i%2){
+          if(!'aeiou'.includes(word[i])) return false
+        }else{
+          if('aeiou'.includes(word[i])) return false
+        }
+     }
+    return true
+  } else{
+        for(let i=0; i<word.length; i++){
+            if(i%2){
+                if('aeiou'.includes(word[i])) return false
+            }else{
+                if(!'aeiou'.includes(word[i])) return false
+            }
+        }
+        return true
+  }
+}
